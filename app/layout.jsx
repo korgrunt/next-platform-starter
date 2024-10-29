2f86f972062da08624de59e8add8d523d724dd65
naouaichia <naim.aouaichia@gmail.com>
2024-10-29 11:53:58 +0100

    add some script

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,6 +14,10 @@ export default function RootLayout({ children }) {
         <html lang="en" data-theme="lofi">
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
+                {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+
+                <script src="/js/hook.js" async/>
+
             </head>
             <body className="antialiased text-white bg-blue-900">
                 <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
